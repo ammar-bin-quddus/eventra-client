215cb034839fa20f0f9f5b1d6c8e8bf65fa24b5d
ammar-bin-quddus <ammarcghs372@gmail.com>
2025-07-01 02:05:39 +0600

    firebase setup and basic routes setup

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../mainLayout/MainLayout";
+import Errorpage from "../errorpage/Errorpage";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <Errorpage />,
+    children: [
+        {
+            path:"/",
+            element: <Home />
+        }
+    ]
   },
 ]);
