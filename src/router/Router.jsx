9266eaf549942640fc725eb4c6a5d182e24e1a7d
ammar-bin-quddus <ammarcghs372@gmail.com>
2025-07-01 00:10:45 +0600

    project setup

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../mainLayout/MainLayout";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+]);
